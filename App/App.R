539d18e120e32a3cd16dbdd52ffb6c122188288c
Jalasalmi <juho.alasalmi@ptt.fi>
2018-09-30 18:46:09 +0200

    Initial commit

---
+++ App/App.R
@@ -0,0 +1,14 @@
+
+
+
+ui <- fluidPage(
+  
+  titlePanel("Linear Approximations of real-valued function on real line")
+)
+
+     
+
+server <- function(input, output, session) {}
+
+
+shinyApp(ui = ui, server = server)
